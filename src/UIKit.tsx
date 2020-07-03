dc509a28943ef7d3ad6cbac2686980b6f6f53953
Playra <raoffonom@icloud.com>
2020-07-03 14:53:51 +0500

    Tabs

--- src/UIKit.tsx
+++ src/UIKit.tsx
@@ -16,7 +16,9 @@ import {
   ButtonIconCircle,
   ButtonComments,
   ButtonDeveloperSub,
-  Star
+  Star,
+  TabDeveloper,
+  TabCompany
 } from './components'
 import { black, white } from './components/constants'
 
@@ -39,6 +41,15 @@ const UIKit = () => {
       <ScrollView
         style={[scrollView, { backgroundColor: dark ? black : white }]}
       >
+        <View style={{ alignItems: 'center' }}>
+          <Txt h0 title="Tabs" />
+          <Space height={30} />
+          <TabDeveloper />
+          <Space height={30} />
+          <TabCompany />
+          <Space height={90} />
+        </View>
+
         <View style={{ alignItems: 'center' }}>
           <Txt h0 title="Buttons" />
           <Space height={30} />
